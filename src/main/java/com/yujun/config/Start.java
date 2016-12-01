9251bb2fa37a180f1ea2a3f6421d873d9c673bd4
yujun <handsomemanyujun@sina.com>
2016-12-01 22:31:12 +0800

    可跑

--- src/main/java/com/yujun/config/Start.java
+++ src/main/java/com/yujun/config/Start.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @PropertySource("classpath:config.properties")
@@ -22,22 +24,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Start {
 	@RequestMapping("/")
-	 @ResponseBody
+	@ResponseBody
     String home() {
         return "Hello World!";
     }
 	
+	
 	@RequestMapping("/hello")  
 	public String hello(Map<String, Object> model){  
-	    List<String> l= new ArrayList(); 
-	    l.add("哈喽，hadoop");  
-	    l.add("哈喽，hbase");  
-	    l.add("哈喽，hive");  
-	    l.add("哈喽，pig");  
-	    l.add("哈喽，zookeeper");  
-	    l.add("哈喽，三劫散仙");  
-	    //将数据存放map里面，可以直接在velocity页面，使用key访问  
-	    model.put("data",l);  
+	    List<String> list= new ArrayList(); 
+	    list.add("我要我们在一起");
+	    model.put("data",list);  
 	  
 	    return "hello";  
 	}  
