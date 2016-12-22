837ce5c6ad2cc1bba5276e37cf50cdd1c843d060
handsomemanyujun <65177582@qq.com>
2016-12-22 22:25:49 +0800

    体检

--- src/main/java/com/yujun/config/Start.java
+++ src/main/java/com/yujun/config/Start.java
@@ -1,30 +1,28 @@
 package com.yujun.config;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 @EnableScheduling
 @ComponentScan("com.yujun")
 @EnableAutoConfiguration 
-public class Start {
+public class Start implements EmbeddedServletContainerCustomizer{
  
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Start.class, args);
     }
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer arg0) {
+		arg0.setPort(80);
+	}
 
 }
