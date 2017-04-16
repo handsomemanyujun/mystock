3ca48c43e6c11df8b94f7f99bcf059a11c4fc31b
yujun <handsomemanyujun@sina.com>
2017-04-16 20:21:12 +0800

    提交

--- src/main/java/com/yujun/core/TradeSchedule.java
+++ src/main/java/com/yujun/core/TradeSchedule.java
@@ -14,6 +14,7 @@ import com.yujun.domain.OrderDO;
 import com.yujun.domain.Setting;
 import com.yujun.domain.StockDO;
 import com.yujun.service.Calculata;
+import com.yujun.util.DateUtil;
 
 @Service
 public class TradeSchedule {
@@ -30,71 +31,97 @@ public class TradeSchedule {
 	@Scheduled(cron="0 0/1 *  * * ? ")
 	public void schedul() {
 		try {
-			for(Map<String,Setting> account : settingService.getAllUserSetting().values()){
-				for(Setting  setting : account.values())  {
-					if(accountService.getAccountByUserId(setting.getUserId())==null ) {
-						log.info("该账户不存在，"+ setting.getUserId());
-						continue;
-					}
-					
-					if(setting.getStatus() ==0) {
-						continue;
-					}
-					
-					log.info("\n\n开始进入自动下单程序："+ setting.getCode());
-					
-					
-					String zqCode 			= setting.getCode();
-					List<StockDO>	holdings= stockClient.queryStockDO(setting.getUserId(), setting.getCode());
-					if(holdings==null || holdings.size()==0) {
-						log.info("该账户已经不持有这个股票，");
-						continue;
-					}
-					
-					OnlinePriceDO online 	= stockClient.queryMarket(setting.getUserId(), setting.getCode());
-					Map<String,StockDO> lh 	= calculata.getTradeOrder(setting.getType(),setting.buildStockDO(),holdings.get(0), online,new Float(setting.getRate()).intValue());
-					
-					StockDO target = lh.get("low");	// 订单买入价
-					if(target !=null) {
-						OrderDO orderDO = stockClient.haveDelegate(setting.getUserId(),true,zqCode);
-						if(orderDO !=null) {
-							if(!isTest && !orderDO.getPrice().equals(target.getAvaPrice())) {
-								stockClient.cancleOrder(setting.getUserId(),orderDO);
-							} 
-						} else {
-							orderDO = new OrderDO();
-							orderDO.setAmount(((Math.abs(target.getAmount()-holdings.get(0).getAmount()))/100)*100);
-							orderDO.setBuy(true);
-							orderDO.setPrice(target.getAvaPrice());
-							orderDO.setZqCode(zqCode);
-							if(!isTest) {
-								stockClient.crateOrder(setting.getUserId(),orderDO);
-							}
-						}
-					}
-					target = lh.get("high");	// 订单卖出价
-					if(target !=null) {
-						OrderDO orderDO = stockClient.haveDelegate(setting.getUserId(),false,zqCode);
-						if(orderDO !=null) {
-							if(!isTest && !orderDO.getPrice().equals(target.getAvaPrice())) {
-								stockClient.cancleOrder(setting.getUserId(),orderDO);
-							} 
-						} else {
-							orderDO = new OrderDO();
-							orderDO.setAmount(((Math.abs(holdings.get(0).getAmount()-target.getAmount()))/100)*100);
-							orderDO.setBuy(false);
-							orderDO.setPrice(target.getAvaPrice());
-							orderDO.setZqCode(zqCode);
-							if(!isTest) {
-								stockClient.crateOrder(setting.getUserId(),orderDO);
-							}
-						}
-					}
-					
+			if (!isTest && DateUtil.isOpenTradeTime()) {
+				if(DateUtil.needCancelOrder()) {	// 收市前最后一分钟，测单
+					cancle();
+				} else {
+					orders();
 				}
+			} else {
+				orders();
 			}
-		} catch (Exception e) {
-			log.error("schedul error",e);
+		} catch(Exception e) {
+			log.info("error, schedul" ,e);
+		}
+	}
+		
+	public void cancle() throws Exception {
+		log.info("\n\n开始撤单");
+		for (Map<String, Setting> account : settingService.getAllUserSetting().values()) {
+			for (Setting setting : account.values()) { 
+				OrderDO orderDO = stockClient.haveDelegate(setting.getUserId(), true, setting.getCode());
+				stockClient.cancleOrder(setting.getUserId(),orderDO);
+			}
+		}
+	}
+	
+	public void orders() throws Exception {
+		for (Map<String, Setting> account : settingService.getAllUserSetting()
+				.values()) {
+			for (Setting setting : account.values()) {
+				if (accountService.getAccountByUserId(setting.getUserId()) == null) {
+					log.info("该账户不存在，" + setting.getUserId());
+					continue;
+				}
+
+				if (setting.getStatus() == 0) {
+					continue;
+				}
+
+				log.info("\n\n开始进入自动下单程序：" + setting.getCode());
+
+				String zqCode = setting.getCode();
+				List<StockDO> holdings = stockClient.queryStockDO(setting.getUserId(), setting.getCode());
+				if (holdings == null || holdings.size() == 0) {
+					log.info("该账户已经不持有这个股票，");
+					continue;
+				}
+
+				OnlinePriceDO online = stockClient.queryMarket(setting.getUserId(), setting.getCode());
+				Map<String, StockDO> lh = calculata.getTradeOrder(setting.getType(), setting.buildStockDO(),
+						holdings.get(0), online, new Float(setting.getRate()).intValue());
+
+				StockDO target = lh.get("low"); // 订单买入价
+				if (target != null) {
+					OrderDO orderDO = stockClient.haveDelegate(setting.getUserId(), true, zqCode);
+					if (orderDO != null) {
+						if (!isTest && !orderDO.getPrice().equals(target.getAvaPrice())) {
+							stockClient.cancleOrder(setting.getUserId(), orderDO);
+						}
+					} else {
+						orderDO = new OrderDO();
+						orderDO.setAmount(((Math.abs(target.getAmount()
+								- holdings.get(0).getAmount())) / 100) * 100);
+						orderDO.setBuy(true);
+						orderDO.setPrice(target.getAvaPrice());
+						orderDO.setZqCode(zqCode);
+						if (!isTest) {
+							stockClient.crateOrder(setting.getUserId(), orderDO);
+						}
+					}
+				}
+				target = lh.get("high"); // 订单卖出价
+				if (target != null) {
+					OrderDO orderDO = stockClient.haveDelegate(setting.getUserId(), false, zqCode);
+					if (orderDO != null) {
+						if (!isTest&& !orderDO.getPrice().equals(target.getAvaPrice())) {
+							stockClient.cancleOrder(setting.getUserId(),
+									orderDO);
+						}
+					} else {
+						orderDO = new OrderDO();
+						orderDO.setAmount(((Math.abs(holdings.get(0)
+								.getAmount() - target.getAmount())) / 100) * 100);
+						orderDO.setBuy(false);
+						orderDO.setPrice(target.getAvaPrice());
+						orderDO.setZqCode(zqCode);
+						if (!isTest) {
+							stockClient.crateOrder(setting.getUserId(), orderDO);
+						}
+					}
+				}
+
+			}
 		}
 	}
 }
